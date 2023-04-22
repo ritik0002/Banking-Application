0743926ef803049ff48b5f6a4b44d84a783648a8
ritik0002 <ritik_d@hotmail.com>
2023-04-22 21:10:58 +0100

    static url error bruh

--- bankingProject/settings.py
+++ bankingProject/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
